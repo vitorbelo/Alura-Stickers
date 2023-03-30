034d8ac07e41ff39d2c583d9cb5e111e27ab3fb1
Vitor Belo <vitorbelo99@gmail.com>
2023-03-30 19:12:17 -0300

    documentacao

--- src/App.java
+++ src/App.java
@@ -10,6 +10,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // -> Conexão HTTP e Busca [TOP FILMS]
+        // -> Documentação de como fazer um Request no Java: https://docs.oracle.com/en/java/javase/17/docs/api/java.net.http/java/net/http/HttpRequest.html
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -19,6 +20,7 @@ public class App {
         String body = response.body();
 
         // -> Extração dados
+        // -> Biblioteca que faz parse de Json: https://github.com/FasterXML/jackson
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
